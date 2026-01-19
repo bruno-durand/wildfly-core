51b9a57e351e2fa326fadfabfe64ac3f7baf1165
Bruno Durand <bruno.z.durand@gmail.com>
2026-01-19 14:34:22 +0100

    update undertow

--- controller/src/main/java/org/jboss/as/controller/management/BaseHttpInterfaceAddStepHandler.java
+++ controller/src/main/java/org/jboss/as/controller/management/BaseHttpInterfaceAddStepHandler.java
@@ -30,6 +30,11 @@ import org.xnio.OptionMap.Builder;
  */
 public abstract class BaseHttpInterfaceAddStepHandler extends ManagementInterfaceAddStepHandler {
 
+    private static final String PROPERTY_BASE = "org.wildfly.management.";
+
+    public static final long DEFAULT_UPLOAD_LIMIT = 1073741824L;
+    public static final String DEFAULT_UPLOAD_LIMIT_PROPERTY = PROPERTY_BASE + "upload.limit";
+
     protected static final String HTTP_AUTHENTICATION_FACTORY_CAPABILITY = "org.wildfly.security.http-authentication-factory";
     protected static final String SASL_AUTHENTICATION_FACTORY_CAPABILITY = "org.wildfly.security.sasl-authentication-factory";
     protected static final String SSL_CONTEXT_CAPABILITY = "org.wildfly.security.ssl-context";
